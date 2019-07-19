3f567a105a0e1dcaf91f5bf6abec74c8129548aa
Kevin Jiah-Chih Liao <me@liaokev.in>
2019-07-19 09:19:24 -0700

    Remove loading of remote config.

--- src/background/config-manager.ts
+++ src/background/config-manager.ts
@@ -1,30 +1,26 @@
-import {readText} from './utils/network';
-import {parseArray} from '../utils/text';
-import {getDuration} from '../utils/time';
-import {parseInversionFixes} from '../generators/css-filter';
-import {parseDynamicThemeFixes} from '../generators/dynamic-theme';
-import {parseStaticThemes} from '../generators/static-theme';
-import {InversionFix, StaticTheme, DynamicThemeFix} from '../definitions';
+import { readText } from './utils/network';
+import { parseArray } from '../utils/text';
+import { getDuration } from '../utils/time';
+import { parseInversionFixes } from '../generators/css-filter';
+import { parseDynamicThemeFixes } from '../generators/dynamic-theme';
+import { parseStaticThemes } from '../generators/static-theme';
+import { InversionFix, StaticTheme, DynamicThemeFix } from '../definitions';
 
 const CONFIG_URLs = {
     darkSites: {
-        remote: 'https://raw.githubusercontent.com/darkreader/darkreader/master/src/config/dark-sites.config',
         local: '../config/dark-sites.config',
     },
     dynamicThemeFixes: {
-        remote: 'https://raw.githubusercontent.com/darkreader/darkreader/master/src/config/dynamic-theme-fixes.config',
         local: '../config/dynamic-theme-fixes.config',
     },
     inversionFixes: {
-        remote: 'https://raw.githubusercontent.com/darkreader/darkreader/master/src/config/inversion-fixes.config',
         local: '../config/inversion-fixes.config',
     },
     staticThemes: {
-        remote: 'https://raw.githubusercontent.com/darkreader/darkreader/master/src/config/static-themes.config',
         local: '../config/static-themes.config',
     },
 };
-const REMOTE_TIMEOUT_MS = getDuration({seconds: 10});
+const REMOTE_TIMEOUT_MS = getDuration({ seconds: 10 });
 
 export default class ConfigManager {
     DARK_SITES?: string[];
@@ -50,33 +46,19 @@ export default class ConfigManager {
         name,
         local,
         localURL,
-        remoteURL,
         success,
     }) {
         let $config: string;
-        const loadLocal = async () => await readText({url: localURL});
-        if (local) {
-            $config = await loadLocal();
-        } else {
-            try {
-                $config = await readText({
-                    url: `${remoteURL}?nocache=${Date.now()}`,
-                    timeout: REMOTE_TIMEOUT_MS
-                });
-            } catch (err) {
-                console.error(`${name} remote load error`, err);
-                $config = await loadLocal();
-            }
-        }
+        const loadLocal = async () => await readText({ url: localURL });
+        $config = await loadLocal();
         success($config);
     }
 
-    private async loadDarkSites({local}) {
+    private async loadDarkSites({ local }) {
         await this.loadConfig({
             name: 'Dark Sites',
             local,
             localURL: CONFIG_URLs.darkSites.local,
-            remoteURL: CONFIG_URLs.darkSites.remote,
             success: ($sites) => {
                 this.raw.darkSites = $sites;
                 this.handleDarkSites();
@@ -84,12 +66,11 @@ export default class ConfigManager {
         });
     }
 
-    private async loadDynamicThemeFixes({local}) {
+    private async loadDynamicThemeFixes({ local }) {
         await this.loadConfig({
             name: 'Dynamic Theme Fixes',
             local,
             localURL: CONFIG_URLs.dynamicThemeFixes.local,
-            remoteURL: CONFIG_URLs.dynamicThemeFixes.remote,
             success: ($fixes) => {
                 this.raw.dynamicThemeFixes = $fixes;
                 this.handleDynamicThemeFixes();
@@ -97,12 +78,11 @@ export default class ConfigManager {
         });
     }
 
-    private async loadInversionFixes({local}) {
+    private async loadInversionFixes({ local }) {
         await this.loadConfig({
             name: 'Inversion Fixes',
             local,
             localURL: CONFIG_URLs.inversionFixes.local,
-            remoteURL: CONFIG_URLs.inversionFixes.remote,
             success: ($fixes) => {
                 this.raw.inversionFixes = $fixes;
                 this.handleInversionFixes();
@@ -110,12 +90,11 @@ export default class ConfigManager {
         });
     }
 
-    private async loadStaticThemes({local}) {
+    private async loadStaticThemes({ local }) {
         await this.loadConfig({
             name: 'Static Themes',
             local,
             localURL: CONFIG_URLs.staticThemes.local,
-            remoteURL: CONFIG_URLs.staticThemes.remote,
             success: ($themes) => {
                 this.raw.staticThemes = $themes;
                 this.handleStaticThemes();
@@ -123,7 +102,7 @@ export default class ConfigManager {
         });
     }
 
-    async load(config: {local: boolean}) {
+    async load(config: { local: boolean }) {
         await Promise.all([
             this.loadDarkSites(config),
             this.loadDynamicThemeFixes(config),
